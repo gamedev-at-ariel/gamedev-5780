cfe2e32b711fbf2f9b98fbe9845cd5ac221aea4a
Erel Segal-Halevi <erelsgl@gmail.com>
2020-02-28 10:34:56 +0200

    edit pdf files

--- docx-to-pdf.bat
+++ docx-to-pdf.bat
@@ -1,1 +1,6 @@
-find . -name "*.docx" | xargs -L 1 docx2pdf
+docx2pdf.exe .\01-design-inspiration\
+docx2pdf.exe .\02-design-formal\
+docx2pdf.exe .\03-design-dramatic\
+docx2pdf.exe .\04-design-dynamic\
+docx2pdf.exe .\05-design-prototyping\
+docx2pdf.exe .\06-design-tuning\
