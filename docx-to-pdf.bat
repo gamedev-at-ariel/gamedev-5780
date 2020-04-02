c25066923fa4f229f3788a74173e5a2ddbc09dcf
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-02 14:39:50 +0300

    week 3 homework

--- docx-to-pdf.bat
+++ docx-to-pdf.bat
@@ -2,5 +2,8 @@ docx2pdf.exe .\01-design-inspiration\
 docx2pdf.exe .\02-design-formal\
 docx2pdf.exe .\03-design-dramatic\
 docx2pdf.exe .\04-design-dynamic\
-docx2pdf.exe .\05-design-prototyping\
-docx2pdf.exe .\06-design-tuning\
+docx2pdf.exe .\05-unity-basics\
+docx2pdf.exe .\06-unity-collisions\
+docx2pdf.exe .\07-unity-animations\
+docx2pdf.exe .\11-design-prototyping\
+docx2pdf.exe .\12-design-tuning\
