c8ce37f2a338dba4f66e30245d2a6888a2bd5092
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-20 16:56:23 +0300

    make pdf

--- docx-to-pdf-design.bat
+++ docx-to-pdf-design.bat
@@ -2,7 +2,4 @@ docx2pdf.exe .\01-design-inspiration\
 docx2pdf.exe .\02-design-formal\
 docx2pdf.exe .\03-design-dramatic\
 docx2pdf.exe .\04-design-dynamic\
-docx2pdf.exe .\05-unity-basics\
-docx2pdf.exe .\06-unity-collisions\
-docx2pdf.exe .\07-unity-art\
 docx2pdf.exe .\12-design-tuning\
---
+++ docx-to-pdf-unity.bat
@@ -0,0 +1,4 @@
+docx2pdf.exe .\05-unity-basics\
+docx2pdf.exe .\06-unity-collisions\
+docx2pdf.exe .\07-unity-art\
+docx2pdf.exe .\08-unity-physics\
