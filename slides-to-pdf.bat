720a5e0804e21828b23dd709c7c0dc780f89bc14
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-30 00:04:47 +0300

    slides to PDF (Thanks again, Arbel!)

---
+++ slides-to-pdf.bat
@@ -0,0 +1,11 @@
+cd 05-unity-basics
+call decktape -s 1280x800 automatic slides.html slides.pdf
+cd ../06-unity-collisions
+call decktape -s 1280x800 automatic slides.html slides.pdf
+cd ../07-unity-art
+call decktape -s 1280x800 automatic slides.html slides.pdf
+cd ../08-unity-physics
+call decktape -s 1280x800 automatic slides.html slides.pdf
+cd ../09-unity-3dgames
+call decktape -s 1280x800 automatic slides.html slides.pdf
+pause
