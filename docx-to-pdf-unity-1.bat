0607e57c9f8e2acb064f67ccfcb4272838445279
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-30 19:21:17 +0300

    Reorganize 3D text files and PDFs

---
+++ docx-to-pdf-unity-1.bat
@@ -0,0 +1,2 @@
+docx2pdf.exe .\05-unity-basics\
+docx2pdf.exe .\06-unity-collisions\
--- docx-to-pdf-unity-2.bat
+++ docx-to-pdf-unity-2.bat
@@ -1,5 +1,3 @@
-docx2pdf.exe .\05-unity-basics\
-docx2pdf.exe .\06-unity-collisions\
 docx2pdf.exe .\07-unity-art\
 docx2pdf.exe .\08-unity-physics\
 docx2pdf.exe .\09-unity-3dgames\
