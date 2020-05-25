7fb4095031a3b0836fb7288a84a43dc545222ea2
Erel Segal-Halevi <erelsgl@gmail.com>
2020-05-25 10:44:56 +0300

    readme and batches

--- docx-to-pdf-unity-2.bat
+++ docx-to-pdf-unity-2.bat
@@ -1,3 +1,3 @@
-docx2pdf.exe .\07-unity-art\
 docx2pdf.exe .\08-unity-physics\
-docx2pdf.exe .\09-unity-3dgames\
+docx2pdf.exe .\09-unity-tilemap-pathfinding\
+docx2pdf.exe .\10-unity-3d-terrain-ai\
